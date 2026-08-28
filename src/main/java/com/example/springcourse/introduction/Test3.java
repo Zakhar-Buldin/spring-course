701e93876b88b7777ffbfc4e3966bf523e163d8b
Zakhar <buldin-zakhar@mail.ru>
2026-08-29 01:54:32 +0300

    DI: внедрение строк и других значений из properties файла (при помощи сеттеров)

--- src/main/java/com/example/springcourse/introduction/Test3.java
+++ src/main/java/com/example/springcourse/introduction/Test3.java
@@ -7,20 +7,23 @@ public class Test3 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); //Spring Container
 
         /*
-         Зависимости для setSurname(String surname) и setAge(int age):
-
-            <bean id="myPerson"
-                  class="com.example.springcourse.introduction.Person">
-                <property name="pet" ref="myPet"/>
-                <property name="surname" value="Buldin"/>
-                <property name="age" value="21"/>
-
-            </bean>
+        DI: внедрение зависимостей и других значений из myApp.properties
+                person.surname = Perova
+                person.name = 19
 
         При создании класса Person автоматически создается объект класса Pet, и в Person передается ссылка на этот объект.
-        Дальше при помощи сеттеров полям класса Person присваются значения (value): "Buldin" и 21
+        Дальше при помощи сеттеров полям класса Person присваются значения (value), которые берутся из properties файла.
 
+        <context:property-placeholder location="classpath:myApp.properties"/> (указываем путь к файлу со свойствами)
+        <bean id="myPerson"
+              class="com.example.springcourse.introduction.Person">
+            <property name="pet" ref="myPet"/>
+            <property name="surname" value="${person.surname}"/>
+            <property name="age" value="${person.age}"/>
+        </bean>
 
+        Называть свойства в properties файле можно как угодно, необязательно "person.age и person.surnamw"
+        Хоть "abc". Это просто хороший тон!
          */
 
         Person person = context.getBean("myPerson", Person.class);
@@ -30,11 +33,11 @@ public class Test3 {
                 Cat myPet = new Cat();
                 Person myPerson = new Person();
                 Person.setPet(myPet);
-                Person.setSurname("Buldin");
-                Person.setAge(21); # ("21" -> 21)
+                Person.setSurname("Perova");
+                Person.setAge(19);
          */
-        person.callYourPet();
 
+        person.callYourPet();
         System.out.println(person.getSurname());
         System.out.println(person.getAge());
 
